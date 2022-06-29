6c759e77a20a20af71d0cfbb3d90737bb3d45001
escc1122 <escc1122@gamil.com>
2022-06-29 22:15:45 +0800

    Fix 反射寫法

--- src/main/java/Activity/Action/ActionFactory.java
+++ src/main/java/Activity/Action/ActionFactory.java
@@ -1,15 +1,21 @@
 package Activity.Action;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class ActionFactory {
-    public static ActionStrategy get(String type){
+    public static ActionStrategy get(String type) {
         ActionStrategy actionStrategy = null;
         try {
-            actionStrategy =  (ActionStrategy)Class.forName(type).newInstance();
+            actionStrategy = (ActionStrategy) Class.forName(type).getDeclaredConstructor().newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
         return actionStrategy;
--- src/main/java/Activity/Condition/ConditionFactory.java
+++ src/main/java/Activity/Condition/ConditionFactory.java
@@ -1,15 +1,21 @@
 package Activity.Condition;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class ConditionFactory {
-    public static ConditionStrategy get(String type){
+    public static ConditionStrategy get(String type) {
         ConditionStrategy conditionStrategy = null;
         try {
-            conditionStrategy = (ConditionStrategy)Class.forName(type).newInstance();
+            conditionStrategy = (ConditionStrategy) Class.forName(type).getDeclaredConstructor().newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
         return conditionStrategy;
